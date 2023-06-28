031ad2e45387695e652e4d75b10ca2097669c245
GabrielRGrilo <gabrielrussog@hotmail.com>
2023-06-28 17:06:54 -0300

    hashed password commit

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,9 @@
+const {hash} = require('bcryptjs')
+
 const AppError = require("../utils/AppError");
 
 
-
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
     /*
@@ -12,14 +14,23 @@ class UsersController {
     * delete - DELETE para remover um registro.     
      */
 
-    create (request, response) {
+   async create (request, response) {
         const { name, email , password} = request.body;
 
-        if(!name) {
-            throw new AppError("O nome é obrigatorio.")
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email =(?)", [email])
+
+        if(checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.")
         }
 
-        response.status(201).json({name, email, password})
+        const hashedPassword = await hash(password,8)
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)",
+        [name, email, hashedPassword]
+        );
+
+        return response.status(201).json();
     }
 
 }
