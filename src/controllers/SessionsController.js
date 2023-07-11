618ebfcc51882c1a9aae3df222db030bf5156552
GabrielRGrilo <gabrielrussog@hotmail.com>
2023-07-11 11:04:34 -0300

    jwt auth token commit

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
 const {compare} = require("bcryptjs");
+const AppError = require("../utils/AppError")
+const authConfig = require("../configs/auth");
+const {sign} = require("jsonwebtoken");
 
 
 class SessionsController {
@@ -18,8 +20,14 @@ class SessionsController {
         if(!passwordMatched) {
             throw new AppError("E-mail e/ou senha incorreta", 401);
         }
+
+        const { secret, expiresIn} = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
         
-        return response.json(user);
+        return response.json({user, token});
 
     }
 }
